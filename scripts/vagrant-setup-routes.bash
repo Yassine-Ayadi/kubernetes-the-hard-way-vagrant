9c5d97090ccb1156434102fe8e52cc831f5770c5
Yassine-Ayadi <82244304+Yassine-Ayadi@users.noreply.github.com>
2022-07-26 10:56:25 +0100

    Update vagrant-setup-routes.bash

--- scripts/vagrant-setup-routes.bash
+++ scripts/vagrant-setup-routes.bash
@@ -7,20 +7,20 @@ apt-get install -y net-tools
 
 case "$(hostname)" in
 worker-0)
-  route add -net 10.21.0.0/16 gw 10.0.0.21
-  route add -net 10.22.0.0/16 gw 10.0.0.22
+  route add -net 10.21.0.0/23 gw 10.0.0.21
+  route add -net 10.22.0.0/23 gw 10.0.0.22
   ;;
 worker-1)
-  route add -net 10.20.0.0/16 gw 10.0.0.20
-  route add -net 10.22.0.0/16 gw 10.0.0.22
+  route add -net 10.20.0.0/23 gw 10.0.0.20
+  route add -net 10.22.0.0/23 gw 10.0.0.22
   ;;
 worker-2)
-  route add -net 10.20.0.0/16 gw 10.0.0.20
-  route add -net 10.21.0.0/16 gw 10.0.0.21
+  route add -net 10.20.0.0/23 gw 10.0.0.20
+  route add -net 10.21.0.0/23 gw 10.0.0.21
   ;;
 *)
-  route add -net 10.20.0.0/16 gw 10.0.0.20
-  route add -net 10.21.0.0/16 gw 10.0.0.21
-  route add -net 10.22.0.0/16 gw 10.0.0.22
+  route add -net 10.20.0.0/23 gw 10.0.0.20
+  route add -net 10.21.0.0/23 gw 10.0.0.21
+  route add -net 10.22.0.0/23 gw 10.0.0.22
   ;;
 esac
